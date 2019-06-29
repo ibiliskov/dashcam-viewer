696748ac61709f4eb13ad158903f972e434dcf4b
ibiliskov <ivan.biliskov@gmail.com>
2019-06-29 15:39:13 +0200

    Update colors for speed chart

--- src/app/speed-chart/speed-chart.component.ts
+++ src/app/speed-chart/speed-chart.component.ts
@@ -20,12 +20,14 @@ export class SpeedChartComponent implements OnChanges {
   @Input() logs: LogStamp[]
   @Input() maxSpeed: number
   speedValue: am4core.Label
+  darkChartBackground = false
   constructor () {}
 
   ngOnChanges (changes: SimpleChanges): void {
     if (changes.maxSpeed) {
       if (this.logs) {
         this.createSpeedMeterChart()
+        this.darkChartBackground = true
         this.logs = []
       }
     }
@@ -39,10 +41,10 @@ export class SpeedChartComponent implements OnChanges {
     const chart = am4core.create('chartdiv', am4charts.GaugeChart)
     chart.fontSize = 12
     chart.hiddenState.properties.opacity = 0
-    chart.innerRadius = -10
+    chart.innerRadius = -15
     chart.startAngle = -210
     chart.endAngle = 30
-    const axis = chart.xAxes.push(new am4charts.ValueAxis() as any)
+    const axis = chart.xAxes.push(new am4charts.ValueAxis<am4charts.AxisRendererCircular>())
     axis.min = 0
     axis.max = this.maxSpeed > 220 ? this.maxSpeed - this.maxSpeed % 10 + 10 : 220
     axis.renderer.minGridDistance = 10
@@ -65,7 +67,7 @@ export class SpeedChartComponent implements OnChanges {
     range0.value = 0
     range0.endValue = 49
     range0.axisFill.fillOpacity = 1
-    range0.axisFill.fill = am4core.color('#4CAF50')
+    range0.axisFill.fill = am4core.color('#009db6')
     range0.axisFill.zIndex = -1
 
     const range1 = axis.axisRanges.create()
@@ -77,24 +79,17 @@ export class SpeedChartComponent implements OnChanges {
 
     const range2 = axis.axisRanges.create()
     range2.value = 52
-    range2.endValue = 90
+    range2.endValue = 130
     range2.axisFill.fillOpacity = 1
-    range2.axisFill.fill = am4core.color('#4CAF50')
+    range2.axisFill.fill = am4core.color('#009db6')
     range2.axisFill.zIndex = -1
 
     const range3 = axis.axisRanges.create()
-    range3.value = 90
-    range3.endValue = 130
+    range3.value = 130
+    range3.endValue = axis.max
     range3.axisFill.fillOpacity = 1
-    range3.axisFill.fill = am4core.color('#FFEE58')
+    range3.axisFill.fill = am4core.color('#f07629')
     range3.axisFill.zIndex = -1
-
-    const range4 = axis.axisRanges.create()
-    range4.value = 130
-    range4.endValue = axis.max
-    range4.axisFill.fillOpacity = 1
-    range4.axisFill.fill = am4core.color('#FFCA28')
-    range4.axisFill.zIndex = -1
 
     this.speedValue = chart.radarContainer.createChild(am4core.Label)
     this.speedValue.isMeasured = false
@@ -104,6 +99,7 @@ export class SpeedChartComponent implements OnChanges {
     this.speedValue.horizontalCenter = 'middle'
     this.speedValue.verticalCenter = 'bottom'
     this.speedValue.text = '0'
+    this.speedValue.dy = 80
 
     const speedUnit = chart.radarContainer.createChild(am4core.Label)
     speedUnit.isMeasured = false
@@ -113,13 +109,22 @@ export class SpeedChartComponent implements OnChanges {
     speedUnit.horizontalCenter = 'middle'
     speedUnit.verticalCenter = 'top'
     speedUnit.text = 'km/h'
+    speedUnit.dy = 80
 
     this.hand = chart.hands.push(new am4charts.ClockHand())
     this.hand.value = 0
-
-    this.hand.pin.disabled = true
-    this.hand.startWidth = 1
-    this.hand.innerRadius = am4core.percent(60)
-    this.hand.radius = am4core.percent(95)
+    this.hand.pin.radius = 12
+    this.hand.pin.fill = am4core.color('#000000')
+    this.hand.pin.stroke = am4core.color('#f07629')
+    this.hand.pin.strokeWidth = 1
+    this.hand.startWidth = 4
+    this.hand.endWidth = 0
+    this.hand.innerRadius = am4core.percent(-20)
+    this.hand.radius = am4core.percent(90)
+    this.hand.fill = am4core.color('#fff')
+    this.hand.stroke = am4core.color('#f07629')
+    this.hand.strokeLinecap = 'round'
+    this.hand.strokeWidth = 2
+    this.hand.strokeLinejoin = 'miter'
   }
 }
